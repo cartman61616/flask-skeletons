3bbb3e1518027785f48d2c619bb895afeffb8d54
Jonathan Davis <JXD85P7@c02y7066jhd5mbp.dv9119.homedepot.com>
2019-07-10 16:52:35 -0400

    initial commit of skeletons

---
+++ GraphQL/mysql/app/__init__.py
@@ -0,0 +1,40 @@
+from config import Config
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    '''
+        Allows you to get configurations
+        from the config file with the exact environment
+        that you require
+        '''
+    app.config.from_object((get_environment_config()))
+
+    db.init_app(app)
+
+    @app.before_first_request
+    def initialize_database():
+        db.create_all()
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db.session.remove()
+
+    @app.route("/")
+    def hello_world():
+        return "Hello World"
+
+    return app
+
+
+def get_environment_config():
+    if Config.ENV == "PROD":
+        return "config.ProdConfig"
+    if Config.ENV == "DEV":
+        return "config.DevConfig"
